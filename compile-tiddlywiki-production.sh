5bc9e726870cc44462abc1411548bf1a19403a1e
zhangaz1 <zhangaz1@hotmail.com>
2022-01-24 17:42:32 +0800

    add compile.sh

---
+++ compile-tiddlywiki-production.sh
@@ -0,0 +1,25 @@
+SOURCE="TiddlyWiki5-Jermolene"
+TARGET="5-1-23"
+VERSION="5.1.23"
+
+([ -d "$TARGET-client" ] || [ -d "$TARGET-server" ]) && echo "Output folder already exists" && exit 1
+
+rm -rf $SOURCE
+git clone https://github.com/Jermolene/TiddlyWiki5 $SOURCE
+(cd $SOURCE && git checkout v$VERSION) 
+#  tags/v$VERSION
+
+node ./compile-tiddlywiki-production.js $SOURCE $TARGET-client $VERSION client
+node ./compile-tiddlywiki-production.js $SOURCE $TARGET-server $VERSION server
+
+cp -vR $SOURCE/tiddlywiki.js $SOURCE/license $SOURCE/licenses $SOURCE/boot $TARGET-client
+cp -vR $SOURCE/tiddlywiki.js $SOURCE/license $SOURCE/licenses $SOURCE/boot $TARGET-server
+
+zip -rq $TARGET-server/editions.zip $SOURCE/editions $SOURCE/bin
+
+cp -v README.md $TARGET-server
+cp -v README.md $TARGET-client
+
+echo "The 'bin' and 'editions' folders are not required for TiddlyWiki to work correctly as a module. They are both included in a zip file."
+
+# (cd 5-1-22-server && npm publish) && (cd 5-1-22-client && npm publish)
