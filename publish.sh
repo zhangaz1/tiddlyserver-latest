840c98cfe072bcc0d7fc8c90d53cf9b8731ad57c
zhangaz1 <zhangaz1@hotmail.com>
2022-01-24 18:36:13 +0800

    update to tw5.2.1

--- publish.sh
+++ publish.sh
@@ -10,4 +10,7 @@ cd /usr/local/lib/node_modules/tiddlyserver/node_modules && ./update.sh
 **************************
 '
 
-(ssh zy && cd /usr/local/lib/node_modules/tiddlyserver/node_modules && ./update.sh)
+ssh zy "
+cd /usr/local/lib/node_modules/tiddlyserver/node_modules
+./update.sh
+"
