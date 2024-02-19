0daf47cdb1d5590b7c3c92b756d16852336c4ce0
zhangaz1 <zhangaz1@hotmail.com>
2024-02-19 18:34:26 +0800

    update

--- update-global-plugins.sh
+++ update-global-plugins.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-GLOBAL="/Users/zhangaz1/workspace/myPrivate/zhangaz1-pro/tiddlywikis/global/plugins"
+GLOBAL="/Users/zhangaz1/workspace/myPrivate/zhangaz1-pro/tiddlywikis/global"
 
-(cd $GLOBAL && rm -rf tiddlywiki)
-
-cp -R tiddlywiki-production-server/plugins/tiddlywiki/ $GLOBAL/tiddlywiki
+(cd $GLOBAL/plugins && rm -rf tiddlywiki)
+cp -R tiddlywiki-production-server/plugins/tiddlywiki/ $GLOBAL/plugins/tiddlywiki
+(cd $GLOBAL/themes && rm -rf tiddlywiki)
+cp -R tiddlywiki-production-server/plugins/tiddlywiki/ $GLOBAL/themes/tiddlywiki
 
 (cd $GLOBAL && git add --all && git commit -m 'update tiddlywiki-plugins' && git push)
