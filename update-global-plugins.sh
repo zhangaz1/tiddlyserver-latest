221c16d00c9929e36ae4304675ea34b52ac14bf8
zhangaz1 <zhangaz1@hotmail.com>
2024-02-19 19:03:32 +0800

    update tiddlywiki-production

--- update-global-plugins.sh
+++ update-global-plugins.sh
@@ -5,6 +5,6 @@ GLOBAL="/Users/zhangaz1/workspace/myPrivate/zhangaz1-pro/tiddlywikis/global"
 (cd $GLOBAL/plugins && rm -rf tiddlywiki)
 cp -R tiddlywiki-production-server/plugins/tiddlywiki/ $GLOBAL/plugins/tiddlywiki
 (cd $GLOBAL/themes && rm -rf tiddlywiki)
-cp -R tiddlywiki-production-server/plugins/tiddlywiki/ $GLOBAL/themes/tiddlywiki
+cp -R tiddlywiki-production-server/themes/tiddlywiki/ $GLOBAL/themes/tiddlywiki
 
 (cd $GLOBAL && git add --all && git commit -m 'update tiddlywiki-plugins' && git push)
--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-(cd /usr/local/lib/node_modules/tiddlyserver/node_modules && /usr/local/git/bin/git pull)
-(cd /usr/local/lib/node_modules/tiddlyserver/node_modules/tiddlywiki-production-server/plugins && /usr/local/git/bin/git pull)
+(cd /usr/local/lib/node_modules/tiddlyserver/node_modules && /usr/local/git/bin/git pull
+(cd /usr/local/lib/node_modules/tiddlyserver/node_modules/tiddlywiki-production-server/plugins && /usr/local/git/bin/git pull))
+(cd /usr/local/lib/node_modules/tiddlyserver/node_modules/tiddlywiki-production-server/themes && /usr/local/git/bin/git pull)
 pm2 restart wikis
