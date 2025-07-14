966d4fdb0d4f6b2105ef24f99e714bb7e9fbf3e8
zhangaz1 <zhangaz1@hotmail.com>
2025-07-14 20:31:20 +0800

    update tiddlywiki-production

--- compile-tiddlywiki-production.sh
+++ compile-tiddlywiki-production.sh
@@ -5,7 +5,7 @@
 # *** THE NEW BUILD WILL BE PUBLISHED ***
 SOURCE="TiddlyWiki5-Jermolene"
 TARGET="latest"
-VERSION="5.3.6"
+VERSION="5.3.7"
 
 ([ -d "$TARGET-client" ] || [ -d "$TARGET-server" ]) && echo "Output folder already exists" && exit 1
 
